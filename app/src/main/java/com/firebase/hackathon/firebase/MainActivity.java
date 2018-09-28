53356b24dad6c9f23deb58b5a5f6ec675f9eba31
2mylesaway <myles.crouther@gmail.com>
2018-09-28 15:52:36 -0400

    Camera Functionality

--- app/src/main/java/com/firebase/hackathon/firebase/MainActivity.java
+++ app/src/main/java/com/firebase/hackathon/firebase/MainActivity.java
@@ -6,6 +6,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
